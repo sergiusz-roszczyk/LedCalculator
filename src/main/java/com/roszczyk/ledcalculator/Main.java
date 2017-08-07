00ba837d8c74161416e6bfb518045c41e105dd9c
Sergiusz Roszczyk <sergiusz@roszczyk.com>
2017-08-07 23:33:24 +0200

    Budowanie przy użyciu Gradle

--- src/main/java/com/roszczyk/ledcalculator/Main.java
+++ src/main/java/com/roszczyk/ledcalculator/Main.java
@@ -6,11 +6,14 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.net.URL;
+
 public class Main extends Application {
 
     @Override
     public void start( Stage primaryStage ) throws Exception {
-        Parent root = FXMLLoader.load( getClass().getResource( "calculator.fxml" ) );
+        URL resource = getClass().getResource( "/calculator.fxml" );
+        Parent root = FXMLLoader.load( resource );
         primaryStage.setTitle( "Kalkulator krzywej Gamma dla LED" );
         primaryStage.setScene( new Scene( root, 900, 650 ) );
         primaryStage.show();
