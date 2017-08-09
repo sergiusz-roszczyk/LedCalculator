8865b739a99f101ecc80f3963608a55e78984512
Sergiusz Roszczyk <sergiusz@roszczyk.com>
2017-08-09 23:19:48 +0200

    Zmiana składni obliczania współczynnika

--- src/main/java/com/roszczyk/ledcalculator/util/Calculator.kt
+++ src/main/java/com/roszczyk/ledcalculator/util/Calculator.kt
@@ -1,5 +1,5 @@
 package com.roszczyk.ledcalculator.util
 
 fun calculateOutputLevels(inLevels: Int, maxOutLevel: Int, gammaValue: Double): List<Pair<String, Number>> {
-    return (0..inLevels).map { Pair(it.toString(), (Math.pow((it.toFloat() / inLevels.toFloat()).toDouble(), gammaValue) * maxOutLevel + 0.5).toInt()) }
+    return (0..inLevels).map { Pair(it.toString(), Math.ceil(Math.pow((it.div(inLevels.toDouble())), gammaValue).times(maxOutLevel)).toInt()) }
 }
